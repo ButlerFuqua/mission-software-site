aeda45d409a04896acf7e6361e93c9129086db2b
ButlerFuqua <butlerfuqua@gmail.com>
2022-04-01 07:45:36 -1000

    Change banner to have img not bg

--- src/lib/layout/LandingBanner.svelte
+++ src/lib/layout/LandingBanner.svelte
@@ -3,35 +3,45 @@
 </script>
 
 <div class="banner">
-    <div id="bannerImage" />
+    <img
+        id="bannerImage-mobile"
+        src="./cloud-banner-mobile.png"
+        alt="mission software banner mobile"
+    />
+    <img
+        id="bannerImage-desktop"
+        src="./cloud-banner-desktop.png"
+        alt="mission software banner desktop"
+    />
     <div class="content">
-        <section>
-            <!-- <img src="./logo.png" alt="mission software logo" /> -->
-            <h1>
-                <span class="secondary-text">Mission</span>
-                <span class="red-text">Software</span>
-            </h1>
-            <p>
-                A mission to build <span class="red-text">free</span> software
-                for
-                <span class="secondary-text">Christian</span> ministries.
-            </p>
-            <button class="tertiary" on:click={() => goto("/apply")}
-                >Apply</button
-            >
-            <button class="secondary" on:click={() => goto("/support")}
-                >Support</button
-            >
-        </section>
+        <h1>
+            <span class="secondary-text">Mission</span>
+            <span class="red-text">Software</span>
+        </h1>
+        <p>
+            A mission to build <span class="red-text">free</span> software for
+            <span class="secondary-text">Christian</span> ministries.
+        </p>
+        <button class="tertiary" on:click={() => goto("/apply")}>Apply</button>
+        <button class="secondary" on:click={() => goto("/support")}
+            >Support</button
+        >
     </div>
 </div>
 
 <style>
     img {
-        max-width: 100px;
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+    }
+    img#bannerImage-desktop {
+        width: 50%;
     }
     .banner {
         display: flex;
+        justify-content: center;
         background-color: var(--dark-color);
         color: var(--white);
         height: 50vh;
@@ -44,53 +54,35 @@
         z-index: 1;
         padding: 1rem;
     }
-    #bannerImage {
-        height: 100%;
-        width: 100%;
-        /* background: url("./cloud-banner.png"); */
-        position: absolute;
-        z-index: 0;
-        top: 0;
-        left: 0;
-    }
     h1 {
         font-size: 4rem;
     }
 
-    @media (min-width: 2000px) {
-        .banner {
-            height: 70vh !important;
-        }
-    }
-
     @media (min-width: 1850px) {
-        .banner {
-            height: 60vh;
-        }
         h1 {
             font-size: 7rem !important;
         }
     }
 
-    @media (min-width: 800px) {
-        #bannerImage {
-            background: url("./cloud-banner-desktop.png");
-            background-size: 70%;
-            background-repeat: no-repeat;
-            background-position: bottom right;
-        }
+    @media (min-width: 840px) {
         h1 {
             font-size: 6rem !important;
         }
     }
+    @media (min-width: 800px) {
+        #bannerImage-mobile {
+            display: none;
+        }
+        #bannerImage-desktop {
+            display: inherit;
+        }
+    }
 
     @media (max-width: 800px) {
-        #bannerImage {
-            background: url("./cloud-banner-mobile.png");
-            background-size: contain;
-            background-repeat: no-repeat;
-            background-position: bottom;
+        #bannerImage-desktop {
+            display: none;
         }
+
         .content {
             /* text-align: center; */
             width: 100%;
